f86baf87c253295e91ac88898343b2fa438efe7e
Daniil Bataev <daniil.bataev@sbermarket.ru>
2023-03-18 19:08:22 +0300

    add frequency analysis

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,41 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(text string) []string {
+	var words = strings.Fields(text)
+	var counterWords = map[string]int{}
+	for _, word := range words {
+		counterWords[word]++
+	}
+	type counterWordEntries struct {
+		word  string
+		count int
+	}
+	var counterWordsSlice []counterWordEntries
+	for word, count := range counterWords {
+		counterWordsSlice = append(counterWordsSlice, counterWordEntries{word: word, count: count})
+	}
+
+	sort.Slice(counterWordsSlice, func(i, j int) bool {
+		if counterWordsSlice[i].count < counterWordsSlice[j].count {
+			return false
+		}
+		if counterWordsSlice[i].count == counterWordsSlice[j].count {
+			return strings.Compare(counterWordsSlice[i].word, counterWordsSlice[j].word) == -1
+		}
+		return true
+	})
+
+	var result []string
+	for i := 0; i < countTopWords && i < len(counterWords); i++ {
+		result = append(result, counterWordsSlice[i].word)
+	}
+
+	return result
 }
+
+const countTopWords = 10
